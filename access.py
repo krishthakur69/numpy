c920f65dde9ed45efbb7ccf6b12057deab317f83
krishthakur69 <krishthakur4924@gmail.com>
2025-07-11 12:13:35 -0700

    1d array indexing

---
+++ access.py
@@ -0,0 +1,11 @@
+# array [index]  #1d array
+# array[row, column] #2d
+
+import numpy as np
+
+arr = np.array ([10,20,30,40,50])
+
+print(arr[0])  #first element
+print(arr[2])  #third element
+print(arr[-1]) #last element
+
