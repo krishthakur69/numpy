2d46e572c0a5eecc648f661ff83aa58d9eb1b520
krishthakur69 <krishthakur4924@gmail.com>
2025-07-13 04:23:45 -0700

    array functions

---
+++ agg_func.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(np.sum(arr))  # output 150
+print(np.mean(arr))  # output 30.0
+print(np.max(arr))   # output 50 
+print(np.min(arr))   # output 10
+print(np.std(arr))  # output 14.1421
+print(np.var(arr))  # output 200.0
