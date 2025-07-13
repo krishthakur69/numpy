cc2d20f3ca50a98d0c548cebc012285ccbe62fb5
krishthakur69 <krishthakur4924@gmail.com>
2025-07-13 04:33:53 -0700

    to add anything

---
+++ concate.py
@@ -0,0 +1,10 @@
+# np.concatenate((array1, array2), axis = 0)
+
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+new_arr = np.concatenate((arr1, arr2))
+print(new_arr)
+
+# concatenate is used to add anything
