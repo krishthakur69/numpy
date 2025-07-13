47dfbced321eccd76c482e2df0bcffaa56b39154
krishthakur69 <krishthakur4924@gmail.com>
2025-07-13 04:26:20 -0700

    dimensions check [1d, 2d..]

---
+++ ndim.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr_1d = np.array([1,2,3])
+arr_2d = np.array([[1,2,3],
+                   [4,5,6]])
+arr_3d = np.array([[[1,2],[3,4],[5,6]]])
+
+print(arr_1d.ndim)  # 1d
+print(arr_2d.ndim)  #2d
+print(arr_3d.ndim)  #3d
+
