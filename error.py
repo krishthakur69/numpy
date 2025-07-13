19db025e4f48dc1c20de3811e5604d7acfae16ff
krishthakur69 <krishthakur4924@gmail.com>
2025-07-13 04:17:19 -0700

    error in addittion [due to different order ]

---
+++ error.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr1 = np.array([[1,2,3],[4,5,6]])  # 2X3 matrix
+arr2 = np.array([1,2])    # 1d array
+
+result = arr1 + arr2
+print(result)
+
+
+# it can not bbe created because they both have different matrix order...
