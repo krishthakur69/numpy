b786f72f7d8154b1021b221a897ee748205b5bba
krishthakur69 <krishthakur4924@gmail.com>
2025-07-13 04:38:31 -0700

    split array from between in equal parts

---
+++ splitting.py
@@ -0,0 +1,11 @@
+# np.split()
+# divide in equal parts
+
+# np.hsplit() horizontally split krna h to 
+# np.vsplit() vertically split krna h to 
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50,60])
+print(np.split(arr, 2))
+
